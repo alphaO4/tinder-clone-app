d1a911f85901018a0e9cb250c2013b557bd88859
AlphaO4 <jonah.svoboda.2004@gmail.com>
2022-05-20 13:54:27 +0200

    small update

--- Fake people/fake_people.py
+++ Fake people/fake_people.py
@@ -3,7 +3,9 @@ from time import sleep
 
 url = "https://thispersondoesnotexist.com/image"
 
-for i in range(51,200):
+input("How many pic's should be downloaded? ")
+
+for i in range(input):
     response = requests.get(url)
     with open('E:/Tinder/tinder-clone-app/Fake people/fake_people-{}.jpg'.format(i), "wb") as file:
         file.write(response.content)
