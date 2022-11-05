a6d799b7063590e3fdbc86ea358bd1fbf6219b28
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2022-11-05 01:16:26 +0100

    Major changes to the Fake People section.

--- face-gender-detection/SortGender.py
+++ face-gender-detection/SortGender.py
@@ -1,8 +1,112 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#imports
 import os
-import predict_gender
+from predict_gender import predict_gender
+import requests
+from faker import Faker
+from time import sleep
+import json
+import sys
 
-#get all file path's and predict the gender
+#config
+path = "E:/github/tinder-clone-app/Fake people"
+files = [f for f in os.listdir(path) if f.endswith(".jpg")]
 
+fake = Faker()
 
+#download fake people from thispersondoesnotexist.com
+def download_pics(i):
+    url = "https://thispersondoesnotexist.com/image"
+    if i == 0:
+        pics_to_download = input("How many pic's should be downloaded? ")
+    else:
+        pics_to_download = i
+    print("Downloading "+ str(pics_to_download) + " pic's...")
+    for i in range(int(pics_to_download)):
+        response = requests.get(url)
+        with open(
+            "E:/GitHub/tinder-clone-app/Fake people/fake_people-{}.jpg".format(i), "wb"
+        ) as file:
+            file.write(response.content)
+            file.close()
+        print("Downloaded fake_people-{}.jpg".format(i))
+        sleep(2)
+    print("Downloaded " + str(pics_to_download) + " pic's\n")
 
-#move the Woman to the folder "Female" move the Man to the folder "Male"
+#identify the gender of the fake people
+
+def identify_gender(path, files):
+    gender = 0
+    males = 0
+    females = 0
+    # loop through all the files
+    for file in files:
+        full_path = path + "/" + file
+        gender = predict_gender(full_path)
+        if gender == "Female":
+            # delete the file
+            os.rename(full_path, path + "/delete{}.jpg".format(females))
+            females += 1
+
+        else:
+            males += 1
+    delete_files = [f for f in os.listdir(path) if f.endswith(".jpg")]
+    for file in delete_files:
+        full_path = path + "/" + file
+        # if the filename is 'delete' delete the file
+        if file.find ("delete") != -1:
+            # delete the file
+            os.remove(full_path)
+            females += 1
+
+        else:
+            males += 1
+
+    print("Saved " + str(males) + " Males and ignored " + str(females) + " Females\n")
+
+#add fake names and the expected imgURL to a json file
+def add_to_json(path, files, fake):
+    json_file = "E:/Github/tinder-clone-app/Fake people/fake_people.json"
+    input_for_json_file = []
+    # loop through all the files
+    try:
+        os.remove(json_file)
+        print("Deleted old json file\n")
+    except:
+        print("No old json file found\n")
+        print("Adding to json...\n")
+        pass
+    for file in files:
+        #since we are going to upload to Github we can predict the imgURL
+        # the imgURL will have the format "https://raw.githubusercontent.com/alphaO4/tinder-clone-app/master/Fake%20people/"
+        imgURL = "https://raw.githubusercontent.com/alphaO4/tinder-clone-app/master/Fake%20people/"
+        #creat fake male name
+        name = fake.name_male()
+        # add the imgURL to the name
+        imgURL = imgURL + file
+        # add the name and the imgURL to a json file
+        input_for_json_file.append({"name": name, "imgUrl": imgURL})
+        print("Added Name: " + name + "and URL: " + imgURL + " to fake_people.json\n")
+    
+    with open(json_file, "a") as file:
+        json.dump(input_for_json_file, file)
+        file.close()
+
+if __name__ == '__main__':
+    i = sys.argv[1]
+    print("Starting...\n")
+    download_pics(i)
+    print("Downloaded fake people\n")
+    sleep(1)
+    print("Identifying...\n")
+    identify_gender(path, files)
+    print("Identified people\n")
+    sleep(1)
+    add_to_json(path, files, fake)
+    print("Added to json\n")
+    sleep(1)
+    print("Done, exiting...\n")
+    sleep(1)
+    exit()
--- face-gender-detection/predict_gender.py
+++ face-gender-detection/predict_gender.py
@@ -1,3 +1,8 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Credit to https://www.thepythoncode.com/article/gender-detection-using-opencv-in-python 
+
 # Import Libraries
 import cv2
 import numpy as np
@@ -5,19 +10,19 @@ import numpy as np
 
 # The gender model architecture
 # https://drive.google.com/open?id=1W_moLzMlGiELyPxWiYQJ9KFaXroQ_NFQ
-GENDER_MODEL = 'weights/deploy_gender.prototxt'
+GENDER_MODEL = 'E:/Github/tinder-clone-app/face-gender-detection/weights/deploy_gender.prototxt'
 # The gender model pre-trained weights
 # https://drive.google.com/open?id=1AW3WduLk1haTVAxHOkVS_BEzel1WXQHP
-GENDER_PROTO = 'weights/gender_net.caffemodel'
+GENDER_PROTO = 'E:/Github/tinder-clone-app/face-gender-detection/weights/gender_net.caffemodel'
 # Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
 # substraction to eliminate the effect of illunination changes
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 # Represent the gender classes
 GENDER_LIST = ['Male', 'Female']
 # https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
-FACE_PROTO = "weights/deploy.prototxt.txt"
+FACE_PROTO = "E:/Github/tinder-clone-app/face-gender-detection/weights/deploy.prototxt.txt"
 # https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
-FACE_MODEL = "weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
+FACE_MODEL = "E:/Github/tinder-clone-app/face-gender-detection/weights/res10_300x300_ssd_iter_140000_fp16.caffemodel"
 
 # load face Caffe model
 face_net = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
@@ -134,11 +139,4 @@ def predict_gender(input_path: str):
         gender = GENDER_LIST[i]
         gender_confidence_score = gender_preds[0][i]
         # Draw the box
-        return(input_path + gender)
-
-
-
-if __name__ == '__main__':
-    # Parsing command line arguments entered by user
-    import sys
-    predict_gender(sys.argv[1])
+        return(gender)
